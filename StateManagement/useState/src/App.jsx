d68e602f2761c801ff30c435479ab1e28cc5b0b3
dnyaneshwarchaudhary <dnyaneshwar.bhartendu1972@gmail.com>
2023-10-17 10:16:33 +0530

    react router

--- StateManagement/useState/src/App.jsx
+++ StateManagement/useState/src/App.jsx
@@ -1,18 +1,73 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  
+
+function NameList(){
+
+  const [list, setList] = useState(["Ram","Sita"]);
+  const [name, setName] = useState(()=>"Jack");
+
+  const onaddName = () =>{
+
+   setList([...list,name]);
+   setName("");
+
+  }
   return (
-    <>
-      <div>
-      
-      </div>
-    
-    </>
+
+    <div>
+      <ul>
+        {list.map((name) =>(
+
+          <li key={name}>{name}</li>
+
+        ))}
+      </ul>
+      <input 
+        type='text'
+        value={name}
+        onChange={(e)=>(
+          setName(e.target.value)
+        )}
+        >
+      </input>
+      <button
+      onClick={onaddName} >
+          Add Name
+      </button>
+    </div>
   )
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(10)
+
+  function addone(){
+
+    setCount(count+1);
+
+  }
+
+  return (
+
+      <div className="App">
+        <button onClick={addone}>
+          Count = {count}
+        </button>
+      </div>
+
+  )
+}
+
+function App(){
+
+  return (
+    <div>
+      <Counter/>
+      <NameList/>
+    </div>
+  )
+}
+
+export default App;
